27cb5a452e03b2c5e6d53b8b840cb8a1c95272e6
Lucas Ricardo <lucasricardox.o@gmail.com>
2022-09-06 16:45:23 -0300

    Category

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -18,7 +18,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
